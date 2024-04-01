d2e58ac5e94d61a38da97661ffdd47e4b3200b15
raphael-devasia <jinomaliyekkal@Jinos-MacBook-Air.local>
2024-04-01 17:59:34 +0530

    image compressed

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-$("#myCarousel.carousel .carousel-item").each(function () {
-  var minPerSlide = 4;
-  var next = $(this).next();
-  if (!next.length) {
-    next = $(this).siblings(":first");
-  }
-  next.children(":first-child").clone().appendTo($(this));
+// $("#myCarousel.carousel .carousel-item").each(function () {
+//   var minPerSlide = 4;
+//   var next = $(this).next();
+//   if (!next.length) {
+//     next = $(this).siblings(":first");
+//   }
+//   next.children(":first-child").clone().appendTo($(this));
 
-  for (var i = 0; i < minPerSlide; i++) {
-    next = next.next();
-    if (!next.length) {
-      next = $(this).siblings(":first");
-    }
-    next.children(":first-child").clone().appendTo($(this));
-  }
-});
+//   for (var i = 0; i < minPerSlide; i++) {
+//     next = next.next();
+//     if (!next.length) {
+//       next = $(this).siblings(":first");
+//     }
+//     next.children(":first-child").clone().appendTo($(this));
+//   }
+// });
